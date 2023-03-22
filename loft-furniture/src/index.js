7ad656b4713bc1915db9eb16c2a0b731179a2231
aimeeriim <2004.01023@manas.edu.kg>
2023-03-22 20:44:00 +0600

    first

---
+++ loft-furniture/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Footer from './component/Footer/Footer';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Footer/>
+  </React.StrictMode>
+);
